052714f4fc0a4d626d4fab58bd1a79561043a8ea
Laura González Fueyo <lgonzalez@grupointermark.com>
2019-06-10 08:29:14 +0200

    añadir return axios getAllCars

--- 00_start/src/API/carsApi.js
+++ 00_start/src/API/carsApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:3050';
 
 export const getAllCars = () => {
-   axios.get(`${baseUrl}/api/cars`)
+   return axios.get(`${baseUrl}/api/cars`)
    .then((result) => {
        return result.data.map((i) => i);
    });
